45139ea960f59ed59749719bfaa6154ef5d9356e
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-10 12:28:49 +0200

    SideBar

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -7,7 +7,7 @@ function SidebarChat() {
     <div className="sidebarChat">
       <Avatar src="https://image.cnbcfm.com/api/v1/image/105755683-1550853148548gettyimages-109478107.jpeg?v=1581098226&w=1400&h=950" />
       <div className="info">
-        <h2>Name</h2>
+        <span>Name</span>
         <p>Last message</p>
       </div>
     </div>
