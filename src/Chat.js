40892ffdbf2a6c309c24372f84dc9765e058856f
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-10 12:38:34 +0200

    SideBar

--- src/Chat.js
+++ src/Chat.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVert from "@material-ui/icons/MoreVert";
 
 function Chat() {
-  return <div className="chat">I'm the chatt</div>;
+  return (
+    <div className="chat">
+      <div className="chatHeader">
+        <Avatar />
+        <div className="chatHeader_info">
+          <span>Room name</span>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chatHeader_right">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chatBody"></div>
+    </div>
+  );
 }
 
 export default Chat;
