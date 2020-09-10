295683e92f34e577fa8ffc39f3a4e306912526b3
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-10 11:45:19 +0200

    SideBar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./SideBar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { IconButton, Avatar } from "@material-ui/core";
 
 function SideBar() {
-  return <div className="sidebar">Hello nigg</div>;
+  return (
+    <div className="sidebar">
+      <div className="header">
+        <Avatar src="https://upload.wikimedia.org/wikipedia/commons/4/43/Tom_Hardy_by_Gage_Skidmore.jpg"></Avatar>
+        <div className="headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
