10bb1564fdbb82917e124742932e2a2f8c3d56c8
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-16 12:09:46 +0200

    Pusher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.css";
 import SideBar from "./SideBar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("818f4bae4e80379dd079", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("my-channel");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app_body">
