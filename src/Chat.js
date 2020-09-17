707b84d78faed0d3a021686039cd9f4c7a092dd6
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-17 13:15:53 +0200

    pusher

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 
@@ -7,8 +7,24 @@ import AttachFile from "@material-ui/icons/AttachFile";
 import MoreVert from "@material-ui/icons/MoreVert";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
+import axios from "./axios";
 
 function Chat({ messages }) {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await axios.post("/messages/new", {
+      message: input,
+      name: "Ernesto",
+      received: true,
+      timestamp: "just now",
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="chat">
       <div className="chatHeader">
@@ -45,8 +61,15 @@ function Chat({ messages }) {
           <AttachFile />
         </IconButton>
         <form>
-          <input placeholder="Type a message" type="text" />
-          <button type="submit">Send</button>
+          <input
+            placeholder="Type a message"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={sendMessage} type="submit">
+            Send
+          </button>
         </form>
 
         <IconButton>
